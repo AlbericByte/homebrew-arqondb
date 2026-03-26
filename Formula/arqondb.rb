d16d5b23ed48c918310dbb52a3a56c32a81cd272
arqondb-ci <ci@arqondb.io>
2026-03-26 02:23:37 +0000

    arqondb 0.1.5

--- Formula/arqondb.rb
+++ Formula/arqondb.rb
@@ -1,26 +1,26 @@
 class Arqondb < Formula
   desc "Distributed key-value storage engine with LSM-tree, Raft, and vector index"
   homepage "https://github.com/AlbericByte/arqondb"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.4/arqondb-0.1.4-darwin-arm64.tar.gz"
-      sha256 "d65881f4847efb83402ccd7a88974aa2a2fcf0ecbb593b09bb925133f4a5434b"
+      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.5/arqondb-0.1.5-darwin-arm64.tar.gz"
+      sha256 "0a55c98738a294134e53c146d1633918d6a6a1f9d6d429b50438e5051e35373a"
     else
-      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.4/arqondb-0.1.4-darwin-amd64.tar.gz"
-      sha256 "22b48b1eba5f45f6130ed3ad02c79c8a7fdb1ab64dfd9a98bc8d1c5fb3ef9294"
+      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.5/arqondb-0.1.5-darwin-amd64.tar.gz"
+      sha256 "7e2982edd1eee0d9a29ab154b17e09188306c317a9b7358547263e4f66c13379"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.4/arqondb-0.1.4-linux-arm64.tar.gz"
-      sha256 "98f81003d4c83e5706a15595fc5a119ad2115cf65fcf0182419edda53920e671"
+      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.5/arqondb-0.1.5-linux-arm64.tar.gz"
+      sha256 "0a22be8d33bbadfb323102716d5b52ecadbbb49444b6431b3477c6e26d26e7d8"
     else
-      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.4/arqondb-0.1.4-linux-amd64.tar.gz"
-      sha256 "95874e4e063ddd7ab4448fc69bf2979e00c97996026c392f60185f49118fefcd"
+      url "https://github.com/AlbericByte/arqondb/releases/download/v0.1.5/arqondb-0.1.5-linux-amd64.tar.gz"
+      sha256 "7cdf42eb5b52e2d428051308e7e22650d54ff86a4d821f1fe2a462bb68b3f8f5"
     end
   end
 
